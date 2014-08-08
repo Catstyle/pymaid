711ffe823c12c3d211aa1b7fb0ce1db8e48ec135
Catsytle <Catstyle.Lee@gmail.com>
2014-08-08 23:59:09 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from pymaid import __version__
+
+
+setup(
+    name="pymaid",
+    version=__version__,
+    author="Catstyle",
+    author_email="Catstyle.Lee@gmail.com",
+    license="do what the f**k you want",
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        'gevent>=1.0',
+        'protobuf>=2.5',
+    ],
+)
