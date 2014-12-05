e511efa24ecb3ee8ad6bfe9c8d1518c7f141e819
Catstyle <Catstyle.Lee@gmail.com>
2014-12-05 17:48:06 +0800

    add agent.close; refine Connection

--- pymaid/agent.py
+++ pymaid/agent.py
@@ -6,15 +6,13 @@ class ServiceAgent(object):
     __slot__ = ['stub']
 
     def __init__(self, stub, conn):
-        self.stub = stub
-        self.conn = conn
-        self._descriptor = stub.GetDescriptor()
+        self.stub, self.conn, self.controller = stub, conn, Controller()
+        self.get_method_by_name = stub.GetDescriptor().FindMethodByName
+        self.get_request_class = stub.GetRequestClass
 
-    def get_method_by_name(self, name):
-        return self._descriptor.FindMethodByName(name)
-
-    def get_request_class(self, method):
-        return self.stub.GetRequestClass(method)
+    def close(self):
+        self.stub, self.conn, self.controller = None, None, None
+        self.get_method_by_name, self.get_request_class = None, None
 
     def __dir__(self):
         return dir(self.stub)
@@ -26,16 +24,14 @@ class ServiceAgent(object):
 
         def rpc(controller=None, request=None, done=None, conn=None, **kwargs):
             if controller is None:
-                controller = Controller()
+                controller = self.controller
+                controller.Reset()
                 assert conn or self.conn
                 controller.conn = conn or self.conn
 
             if request is None:
                 request_class = self.get_request_class(method_descriptor)
-                if kwargs:
-                    request = request_class(**kwargs)
-                else:
-                    request = request_class()
+                request = request_class(**kwargs)
 
             method = getattr(self.stub, name)
             return method(controller, request, done)
