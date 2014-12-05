e4cf9b63a7967ce751303873a404a7b9769007d2
Catstyle <Catstyle.Lee@gmail.com>
2014-12-05 18:36:26 +0800

    use ares dns resolver under linux platform

--- pymaid/__init__.py
+++ pymaid/__init__.py
@@ -1,16 +1,19 @@
+__all__ = [
+    'ServiceAgent', 'Channel', 'Controller', 'Connection',
+    'Error', 'Warning', 'logger', 'pool'
+]
+
+__version__ = '0.0.1'
+VERSION = tuple(map(int, __version__.split('.')))
+
+import sys
+if sys.platform.startswith('linux'):
+    import os
+    os.environ.setdefault('GEVENT_RESOLVER', 'ares')
+
 from agent import ServiceAgent
 from channel import Channel
 from controller import Controller
 from connection import Connection
 from error import Error, Warning
 from utils import logger, pool
-
-
-__version__ = '0.0.1'
-VERSION = tuple(map(int, __version__.split('.')))
-
-
-__all__ = [
-    'ServiceAgent', 'Channel', 'Controller', 'Connection',
-    'Error', 'Warning', 'logger', 'pool'
-]
