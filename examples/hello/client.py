40325724f30eaee782c9736039f937f1a4c41a1d
Catstyle <Catstyle.Lee@gmail.com>
2014-12-17 17:49:48 +0800

    add some debug info in examples

--- examples/hello/client.py
+++ examples/hello/client.py
@@ -3,6 +3,7 @@ from gevent.pool import Pool
 from pymaid.channel import Channel
 from pymaid.agent import ServiceAgent
 from hello_pb2 import HelloService_Stub
+from pymaid.utils import greenlet_pool
 
 
 def wrapper(pid, n):
@@ -16,6 +17,12 @@ def wrapper(pid, n):
 channel = Channel()
 service = ServiceAgent(HelloService_Stub(channel), conn=None)
 def main():
+    import gc
+    import sys
+    from collections import Counter
+    gc.set_debug(gc.DEBUG_LEAK&gc.DEBUG_UNCOLLECTABLE)
+    gc.enable()
+
     pool = Pool()
     #pool.spawn(wrapper, 111111, 30000)
     for x in xrange(30000):
@@ -27,6 +34,13 @@ def main():
         print len(channel.pending_results)
         print len(channel._outcome_connections)
         print len(channel._income_connections)
+        print pool.size, len(pool.greenlets)
+        print greenlet_pool.size, len(greenlet_pool.greenlets)
+
+        objects = gc.get_objects()
+        print Counter(map(type, objects))
+        print
+        print Counter({type(obj): sys.getsizeof(obj) for obj in objects})
     #assert len(channel.pending_results) == 0, channel.pending_results
     #assert len(channel._outcome_connections) == 0, channel._outcome_connections
     #assert len(channel._income_connections) == 0, channel._income_connections
