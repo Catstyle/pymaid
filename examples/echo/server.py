a12bac227c8a88b6e3c54398e8f7810f1677760e
Catstyle <Catstyle.Lee@gmail.com>
2015-01-13 13:13:18 +0800

    add echo example

---
+++ examples/echo/server.py
@@ -0,0 +1,38 @@
+from pymaid.channel import Channel
+from pymaid.utils import greenlet_pool
+
+from echo_pb2 import Message
+from echo_pb2 import EchoService
+
+
+class EchoServiceImpl(EchoService):
+
+    def echo(self, controller, request, done):
+        response = Message()
+        response.message = request.message
+        done(response)
+
+def main():
+    import gc
+    from collections import Counter
+    gc.set_debug(gc.DEBUG_LEAK&gc.DEBUG_UNCOLLECTABLE)
+    gc.enable()
+
+    channel = Channel()
+    channel.listen("127.0.0.1", 8888)
+    channel.append_service(EchoServiceImpl())
+    #channel.enable_heartbeat(10, 3)
+    try:
+        channel.serve_forever()
+    except:
+        print len(channel.pending_results)
+        print len(channel._outcome_connections)
+        print len(channel._income_connections)
+        print greenlet_pool.size, len(greenlet_pool.greenlets)
+
+        objects = gc.get_objects()
+        print Counter(map(type, objects))
+        print
+
+if __name__ == "__main__":
+    main()
