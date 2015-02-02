8bffb2dd022af41b321eebfbb4c352712f476c00
Catsytle <Catstyle.Lee@gmail.com>
2015-02-03 00:17:43 +0800

    add data_files in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from setuptools import setup, find_packages
 from pymaid import __version__
 
@@ -10,6 +12,10 @@ setup(
     license="do what the f**k you want",
     packages=find_packages(exclude='examples'),
     zip_safe=True,
+    data_files = [
+        (os.path.join(sys.prefix, 'include', 'pymaid', 'pb'),
+         ['pymaid/pb/pymaid.proto']),
+    ],
     install_requires=[
         'gevent>=1.0',
         'protobuf>=2.6',
