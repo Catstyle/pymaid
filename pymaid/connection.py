bb2497cf19a18258be9f0a3439876bd6c81ac703
Catstyle <Catstyle.Lee@gmail.com>
2015-02-04 14:43:28 +0800

    fix broadcast/group; fix controller.SetFailed

--- pymaid/connection.py
+++ pymaid/connection.py
@@ -122,10 +122,16 @@ class Connection(object):
 
         if isinstance(reason, Greenlet):
             reason = reason.exception
-        self.logger.info(
-            '[host|%s][peer|%s] closed with reason: %s',
-            self.sockname, self.peername, reason
-        )
+        if reason:
+            self.logger.warn(
+                '[host|%s][peer|%s] closed with reason: %s',
+                self.sockname, self.peername, reason
+            )
+        else:
+            self.logger.info(
+                '[host|%s][peer|%s] closed with reason: %s',
+                self.sockname, self.peername, reason
+            )
 
         if self._monitor_agent:
             self._monitor_agent.close()
