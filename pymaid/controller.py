0e6eeda9b671d1c15d4b4c9c464a5967aec42add
Catstyle <Catstyle.Lee@gmail.com>
2015-02-06 17:23:55 +0800

    hack controller attribute assignment

--- pymaid/controller.py
+++ pymaid/controller.py
@@ -8,19 +8,24 @@ def __init__(self, *args, **kwargs):
     self.__dict__['broadcast'] = False
     self.__dict__['group'] = None
 
+
 def Reset(self):
     self.Clear()
-    self.broadcast = False
-    self.group = None
+    self.__dict__['broadcast'] = False
+    self.__dict__['group'] = None
+
 
 def Failed(self):
     return self.is_failed
 
+
 def ErrorText(self):
     return self.message
 
+
 def StartCancel(self):
     pass
+
 
 def SetFailed(self, reason):
     self.is_failed = True
@@ -32,19 +37,21 @@ def SetFailed(self, reason):
     else:
         self.message = repr(reason)
 
+
 def IsCanceled(self):
     return self.is_canceled
+
 
 def NotifyOnCancel(self, callback):
     pass
 
-def __setattr__(self, name, value):
-    if name in self.DESCRIPTOR.fields_by_name:
-        super(Controller, self).__setattr__(name, value)
-    elif name in self.__dict__:
+
+def property_setter(controller, name):
+
+    def setter(self, value):
         self.__dict__[name] = value
-    else:
-        raise AttributeError('Assignment not allowed')
+    setattr(controller, 'set_' + name, setter)
+
 
 Controller.__init__ = __init__
 Controller.Reset = Reset
@@ -54,4 +61,7 @@ Controller.StartCancel = StartCancel
 Controller.SetFailed = SetFailed
 Controller.IsCanceled = IsCanceled
 Controller.NotifyOnCancel = NotifyOnCancel
-Controller.__setattr__ = __setattr__
+
+property_setter(Controller, 'conn')
+property_setter(Controller, 'broadcast')
+property_setter(Controller, 'group')
