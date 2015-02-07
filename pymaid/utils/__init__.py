a7576e980454fd5ba37bc2c337b6db157a35cc6b
Catstyle <Catstyle.Lee@gmail.com>
2015-02-07 14:46:25 +0800

    move compiled struct back to Connection

--- pymaid/utils/__init__.py
+++ pymaid/utils/__init__.py
@@ -1,3 +1,2 @@
 from .pool import greenlet_pool
 from .logger import configure_project_logger, logger_wrapper, pymaid_logger_wrapper
-from struct import HEADER, HEADER_LENGTH, LINGER_PACK, header_struct
