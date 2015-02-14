a643ed0f0aa6e975ea52050d1810fd7b97f61213
Catstyle <Catstyle.Lee@gmail.com>
2015-02-14 14:05:31 +0800

    fix setup file packages

--- pymaid/__init__.py
+++ pymaid/__init__.py
@@ -1,41 +1,39 @@
 from __future__ import absolute_import
 __all__ = [
     'ServiceAgent', 'Channel', 'Controller', 'Connection',
-    'Error', 'Warning', 'parser', 'logger', 'pool', 'profiler'
+    'Error', 'Warning', 'parser', 'logger', 'pool',
 ]
 
+
+import sys
+import six
 
 __version__ = '0.2.1'
 VERSION = tuple(map(int, __version__.split('.')))
 
 
-import sys
 platform = sys.platform
 if 'linux' in platform or 'darwin' in platform:
     import os
     if 'GEVENT_RESOLVER' not in os.environ:
         os.environ['GEVENT_RESOLVER'] = 'ares'
-        try:
-            from imp import reload
-        except ImportError:
-            pass
         import gevent
-        reload(gevent)
+        six.moves.reload_module(gevent)
     else:
         gevent_resolver = os.environ['GEVENT_RESOLVER']
         if 'ares' not in gevent_resolver:
             sys.stdout.write(
                 'ares-resolver is better, just `export GEVENT_RESOLVER=ares`\n'
             )
-    if 'cpp' != os.environ.get('PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'):
+    if os.environ.get('PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION') != 'cpp':
         sys.stdout.write(
             'C++ implementation protocol buffer has overall performance, see'
             '`https://github.com/google/protobuf/blob/master/python/README.txt#L84-L105`\n'
         )
-    if '2' != os.environ.get('PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION'):
+    if os.environ.get('PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION') != '2':
         sys.stdout.write(
             'pb>=2.6 new C++ implementation also require to '
-            '`export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2`'
+            '`export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2`\n'
         )
 
 
