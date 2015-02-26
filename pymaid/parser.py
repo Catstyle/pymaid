ae7841ff443a9f6a4ffd6f9cb4ee56b4f95666ff
Catstyle <Catstyle.Lee@gmail.com>
2015-02-26 15:34:26 +0800

    add ParserMeta; fix JSONParser

--- pymaid/parser.py
+++ pymaid/parser.py
@@ -4,6 +4,7 @@ __all__ = [
 ]
 
 import struct
+import six
 try:
     import ujson as json
 except ImportError:
@@ -13,30 +14,68 @@ import pymaid
 from pymaid.error import ParserNotExist
 
 
-class PBParser(object):
-    '''Google Protocol Buffer Parser'''
+parsers = {}
+
+
+class ParserMeta(type):
+
+    def __init__(cls, name, bases, attrs):
+        super(ParserMeta, cls).__init__(name, bases, attrs)
+        if 'parser_type' not in attrs:
+            raise AttributeError('%s has not set `parser_type` attribute')
+        parser_type = attrs['parser_type']
+        assert parser_type not in parsers
+        parsers[parser_type] = cls
+
+
+@six.add_metaclass(ParserMeta)
+class Parser(object):
+
+    parser_type = None
 
     @staticmethod
     def pack_packet(packet):
-        return packet.SerializeToString()
+        raise NotImplementedError
+
+    @staticmethod
+    def unpack_packet(packet_buffer):
+        raise NotImplementedError
+
+
+class PBParser(Parser):
+    '''Google Protocol Buffer Parser'''
+
+    parser_type = 1
+
+    @staticmethod
+    def pack_packet(meta):
+        return meta.SerializeToString()
 
     @staticmethod
     def unpack_packet(packet_buffer):
         controller = pymaid.Controller()
         controller.meta.ParseFromString(packet_buffer)
+        controller.parser_type = PBParser.parser_type
         return controller
 
 
-class JSONParser(object):
-    '''Google Protocol Buffer Parser'''
+class JSONParser(Parser):
+    '''JSON style parser'''
+
+    parser_type = 2
 
     @staticmethod
-    def pack_packet(packet):
-        return json.dumps(packet)
+    def pack_packet(meta):
+        return bytes(
+            json.dumps({field.name: value for field, value in meta.ListFields()}),
+            'utf-8'
+        )
 
     @staticmethod
     def unpack_packet(packet_buffer):
-        return json.loads(packet_buffer)
+        controller = pymaid.Controller(**json.loads(packet_buffer))
+        controller.parser_type = JSONParser.parser_type
+        return controller
 
 
 # packet type
@@ -44,16 +83,8 @@ REQUEST = 1
 RESPONSE = 2
 NOTIFICATION = 3
 
-# parser type
-PB = 1
-JSON = 2
-DEFAULT_PARSER = PB
-
-
-PARSERS = {
-    PB: PBParser,
-    JSON: JSONParser
-}
+DEFAULT_PARSER = PBParser.parser_type
+DEFAULT_PARSER = JSONParser.parser_type
 
 
 HEADER = '!BH'
@@ -64,18 +95,18 @@ unpack_header = HEADER_STRUCT.unpack
 
 
 def get_parser(parser_type):
-    if parser_type not in PARSERS:
+    if parser_type not in parsers:
         raise ParserNotExist(parser_type=parser_type)
-    return PARSERS[parser_type]
+    return parsers[parser_type]
 
 
 def pack_packet(packet, parser_type):
-    if parser_type not in PARSERS:
+    if parser_type not in parsers:
         raise ParserNotExist(parser_type=parser_type)
-    return PARSERS[parser_type].pack_packet(packet)
+    return parsers[parser_type].pack_packet(packet)
 
 
 def unpack_packet(packet_buffer, parser_type):
-    if parser_type not in PARSERS:
+    if parser_type not in parsers:
         raise ParserNotExist(parser_type=parser_type)
-    return PARSERS[parser_type].unpack_packet(packet_buffer)
+    return parsers[parser_type].unpack_packet(packet_buffer)
