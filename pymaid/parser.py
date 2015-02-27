e35f4d1f9daf13b657b69afe85524da7026aedc1
Catstyle <Catstyle.Lee@gmail.com>
2015-02-27 11:39:42 +0800

    fix json parser; fix controller pack content

--- pymaid/parser.py
+++ pymaid/parser.py
@@ -4,6 +4,7 @@ __all__ = [
 ]
 
 import struct
+import collections
 import six
 try:
     import ujson as json
@@ -13,6 +14,17 @@ except ImportError:
 import pymaid
 from pymaid.error import ParserNotExist
 
+
+# packet type
+REQUEST = 1
+RESPONSE = 2
+NOTIFICATION = 3
+
+HEADER = '!BH'
+HEADER_LENGTH = struct.calcsize(HEADER)
+HEADER_STRUCT = struct.Struct(HEADER)
+pack_header = HEADER_STRUCT.pack
+unpack_header = HEADER_STRUCT.unpack
 
 parsers = {}
 
@@ -53,8 +65,7 @@ class PBParser(Parser):
 
     @staticmethod
     def unpack_packet(packet_buffer):
-        controller = pymaid.Controller()
-        controller.meta.ParseFromString(packet_buffer)
+        controller = pymaid.Controller(meta_buffer=packet_buffer)
         controller.parser_type = PBParser.parser_type
         return controller
 
@@ -66,31 +77,19 @@ class JSONParser(Parser):
 
     @staticmethod
     def pack_packet(meta):
-        return bytes(
-            json.dumps({field.name: value for field, value in meta.ListFields()}),
-            'utf-8'
-        )
+        return json.dumps(
+            {field.name: value for field, value in meta.ListFields()},
+            ensure_ascii=False,
+        ).encode('utf-8')
 
     @staticmethod
     def unpack_packet(packet_buffer):
-        controller = pymaid.Controller(**json.loads(packet_buffer))
+        controller = pymaid.Controller(**keys_to_string(json.loads(packet_buffer)))
         controller.parser_type = JSONParser.parser_type
         return controller
 
 
-# packet type
-REQUEST = 1
-RESPONSE = 2
-NOTIFICATION = 3
-
 DEFAULT_PARSER = PBParser.parser_type
-
-
-HEADER = '!BH'
-HEADER_LENGTH = struct.calcsize(HEADER)
-HEADER_STRUCT = struct.Struct(HEADER)
-pack_header = HEADER_STRUCT.pack
-unpack_header = HEADER_STRUCT.unpack
 
 
 def get_parser(parser_type):
@@ -109,3 +108,9 @@ def unpack_packet(packet_buffer, parser_type):
     if parser_type not in parsers:
         raise ParserNotExist(parser_type=parser_type)
     return parsers[parser_type].unpack_packet(packet_buffer)
+
+
+def keys_to_string(data):
+    if not isinstance(data, collections.Mapping):
+        return data
+    return {str(k): keys_to_string(v) for k, v in data.items()}
