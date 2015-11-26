1d3b6fdeae17e42d93de543caa101ee5844783fc
Catstyle <Catstyle.Lee@gmail.com>
2015-11-26 18:47:06 +0800

    added implall decorator, used to check all methods has been implemented

--- pymaid/utils/core.py
+++ pymaid/utils/core.py
@@ -5,3 +5,17 @@ hub = get_hub()
 timer = hub.loop.timer
 io = hub.loop.io
 del hub
+
+
+def implall(service):
+    service_name = service.DESCRIPTOR.name
+    for base in service.__bases__:
+        for method in base.DESCRIPTOR.methods:
+            method_name = method.name
+            base_method = getattr(base, method_name)
+            impl_method = getattr(service, method_name, base_method)
+            if base_method == impl_method:
+                raise RuntimeError(
+                    '%s.%s is not implemented' % (service_name, method_name)
+                )
+    return service
