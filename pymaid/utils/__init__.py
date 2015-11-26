cff38d5b97e0007211d54205b855b4dfc6a43334
Catstyle <Catstyle.Lee@gmail.com>
2015-11-26 18:47:06 +0800

    added implall decorator, used to check all methods has been implemented

--- pymaid/utils/__init__.py
+++ pymaid/utils/__init__.py
@@ -1,3 +1,3 @@
-from .core import timer, io
+from .core import timer, io, implall
 from .pool import greenlet_pool
 from .logger import configure_project_logger, logger_wrapper, pymaid_logger_wrapper
