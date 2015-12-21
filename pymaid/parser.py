4c056b6c5c58a10887536c177824ab41ad028000
Catstyle <Catstyle.Lee@gmail.com>
2015-12-22 01:03:08 +0800

    refined parser, specified parser type for channels; fixed error situation in handle_response

--- pymaid/parser.py
+++ pymaid/parser.py
@@ -1,6 +1,4 @@
-__all__ = [
-    'DEFAULT_PARSER', 'pb_type', 'json_type', 'get_pack', 'get_unpack', 'pack'
-]
+__all__ = ['PBParser', 'JSONParser']
 
 try:
     import ujson as json
@@ -11,55 +9,11 @@ from collections import Mapping
 
 from google.protobuf.message import Message
 
-from .error import RpcError
-
-HEADER = '!BHH'
+HEADER = '!HH'
 HEADER_LENGTH = struct.calcsize(HEADER)
 HEADER_STRUCT = struct.Struct(HEADER)
 pack_header = HEADER_STRUCT.pack
 unpack_header = HEADER_STRUCT.unpack
-
-
-def pack_pb_packet(packet):
-    return packet.SerializeToString()
-
-
-def unpack_pb_packet(packet_buffer, cls):
-    return cls.FromString(packet_buffer)
-
-
-def pack_json_packet(packet):
-    return json.dumps(
-        {field.name: value for field, value in packet.ListFields()},
-        ensure_ascii=False,
-    ).encode('utf-8')
-
-
-def unpack_json_packet(packet_buffer, cls):
-    return cls(**keys_to_string(json.loads(packet_buffer)))
-
-
-packs, unpacks = {}, {}
-ParserNotExist = RpcError.ParserNotExist
-pb_type = 1
-json_type = 2
-DEFAULT_PARSER = pb_type
-packs[pb_type] = pack_pb_packet
-unpacks[pb_type] = unpack_pb_packet
-packs[json_type] = pack_json_packet
-unpacks[json_type] = unpack_json_packet
-
-
-def get_pack(parser_type):
-    if parser_type not in packs:
-        raise ParserNotExist(parser_type=parser_type)
-    return packs[parser_type]
-
-
-def get_unpack(parser_type):
-    if parser_type not in unpacks:
-        raise ParserNotExist(parser_type=parser_type)
-    return unpacks[parser_type]
 
 
 def keys_to_string(data):
@@ -68,14 +22,48 @@ def keys_to_string(data):
     return {str(k): keys_to_string(v) for k, v in data.items()}
 
 
-def pack(meta, content=b'', parser_type=DEFAULT_PARSER):
-    pack_function = get_pack(parser_type)
-    if isinstance(content, Message):
-        content = pack_function(content)
-    else:
-        content = str(content)
-    meta_content = pack_function(meta)
-    return b''.join([
-        pack_header(parser_type, len(meta_content), len(content)),
-        meta_content, content
-    ])
+class BaseParser(object):
+
+    @classmethod
+    def pack_meta(cls, meta, content=b''):
+        if isinstance(content, Message):
+            content = cls.pack(content)
+        else:
+            content = str(content)
+        meta_content = cls.pack(meta)
+        return b''.join([
+            pack_header(len(meta_content), len(content)), meta_content, content
+        ])
+
+    @staticmethod
+    def pack(packet):
+        raise NotImplementedError
+
+    @staticmethod
+    def unpack(packet_buffer, cls):
+        raise NotImplementedError
+
+
+class PBParser(BaseParser):
+
+    @staticmethod
+    def pack(packet):
+        return packet.SerializeToString()
+
+    @staticmethod
+    def unpack(packet_buffer, cls):
+        return cls.FromString(packet_buffer)
+
+
+class JSONParser(BaseParser):
+
+    @staticmethod
+    def pack(packet):
+        return json.dumps(
+            {field.name: value for field, value in packet.ListFields()},
+            ensure_ascii=False,
+        ).encode('utf-8')
+
+    @staticmethod
+    def unpack(packet_buffer, cls):
+        return cls(**keys_to_string(json.loads(packet_buffer)))
--- pymaid/pb/controller.py
+++ pymaid/pb/controller.py
@@ -1,27 +1,19 @@
 __all__ = ['Controller']
-from functools import partial
 
 from google.protobuf.service import RpcController
 
-from pymaid.parser import pack, get_unpack
 from .pymaid_pb2 import Controller as Meta
 
 
 class Controller(RpcController):
 
-    __slots__  = ['meta', 'conn', 'pack', 'unpack']
+    __slots__  = ['meta', 'conn']
 
-    def __init__(self, meta=None, conn=None, parser_type=None, **kwargs):
+    def __init__(self, meta=None, conn=None, **kwargs):
         self.meta, self.conn = meta or Meta(**kwargs), conn
-        if parser_type:
-            self.pack = partial(pack, parser_type=parser_type)
-            self.unpack = get_unpack(parser_type)
-        else:
-            self.pack = self.unpack = None
 
     def Reset(self):
         self.meta.Clear()
-        self.conn = self.pack = self.unpack = None
 
     def Failed(self):
         return self.meta.is_failed
