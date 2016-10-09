6cb3c64c1be35ca6db95fcc7ac25fef3adecf6f0
Catstyle <Catstyle.Lee@gmail.com>
2016-10-09 18:08:48 +0800

    reduced socket send system call

--- pymaid/utils/__init__.py
+++ pymaid/utils/__init__.py
@@ -1,11 +1,13 @@
 from .core import hub, timer, io, implall, greenlet_pool, greenlet_worker
 from .worker import Worker, queue_worker, apply_worker, apply_delay_worker
 from .logger import (
-    create_project_logger, logger_wrapper, trace_service, trace_method
+    create_project_logger, pymaid_logger_wrapper, logger_wrapper,
+    trace_service, trace_method
 )
 
 __all__ = [
     'hub', 'timer', 'io', 'implall', 'greenlet_pool', 'greenlet_worker',
     'Worker', 'queue_worker', 'apply_worker', 'apply_delay_worker',
-    'create_project_logger', 'logger_wrapper', 'trace_service', 'trace_method'
+    'create_project_logger', 'pymaid_logger_wrapper', 'logger_wrapper',
+    'trace_service', 'trace_method'
 ]
