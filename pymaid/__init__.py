f6dfac1483ded2834404821c4a810d34cdccbc52
Catstyle <Catstyle.Lee@gmail.com>
2017-01-05 22:44:29 +0800

    refined load_from_ api

--- pymaid/__init__.py
+++ pymaid/__init__.py
@@ -14,7 +14,7 @@ __all__ = [
     'serve_forever'
 ]
 
-__version__ = '0.3.5.post1'
+__version__ = '0.3.5.post2'
 
 if 'linux' in sys.platform or 'darwin' in sys.platform:
     if 'ares' not in os.environ.get('GEVENT_RESOLVER', ''):
