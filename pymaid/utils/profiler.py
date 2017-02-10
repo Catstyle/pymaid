d348d1a41046848ba95824b57bc007bfb638c5b5
Catstyle <Catstyle.Lee@gmail.com>
2017-02-10 18:54:21 +0800

    refined utils.profiler

--- pymaid/utils/profiler.py
+++ pymaid/utils/profiler.py
@@ -1,13 +1,22 @@
+from inspect import ismodule
+
 from line_profiler import LineProfiler
 
 
 class Profiler(LineProfiler):
+    """Enhance add_module
 
-    def enable_all(self):
-        ''' enable profiler with pymaid injection'''
-        import pymaid
-        self.add_module(pymaid)
-        self.enable_by_count()
+    usage:
+        profiler.add_module(pymaid, add_submodule=True)
+        ...
+        profiler.print_stats()
+    """
+
+    def add_module(self, mod, add_submodule=False):
+        super(Profiler, self).add_module(mod)
+        for item in mod.__dict__.values():
+            if ismodule(item):
+                super(Profiler, self).add_module(item)
 
     def profile(self, func):
         self.add_function(func)
