2306154b9768aba962d3da1d08b3085431f1812a
Catstyle <Catstyle.Lee@gmail.com>
2017-03-29 16:53:47 +0800

    added settings RedisBackend/ZooKeeperBackend; refined setup requires and dump version to 0.3.6.beta2

--- pymaid/__init__.py
+++ pymaid/__init__.py
@@ -15,7 +15,7 @@ __all__ = [
     'conf', 'serve_forever'
 ]
 
-__version__ = '0.3.6.beta1'
+__version__ = '0.3.6.beta2'
 
 if 'linux' in sys.platform or 'darwin' in sys.platform:
     if 'ares' not in os.environ.get('GEVENT_RESOLVER', ''):
