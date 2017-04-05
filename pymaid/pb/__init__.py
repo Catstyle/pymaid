dcfbf9f15f517d24a7087dbdad2263681cc6b41e
Catstyle <Catstyle.Lee@gmail.com>
2017-04-05 11:23:19 +0800

    pep8 format; fixed example/hello/handler_client

--- pymaid/pb/__init__.py
+++ pymaid/pb/__init__.py
@@ -1,10 +1,11 @@
-__all__ = [
-    'PBHandler', 'Listener', 'PBController', 'ServiceStub', 'StubManager',
-    'Void', 'Controller', 'ErrorMessage'
-]
-
 from .listener import Listener
 from .handler import PBHandler
 from .controller import Controller as PBController
 from .stub import ServiceStub, StubManager
 from .pymaid_pb2 import Void, Controller, ErrorMessage
+
+
+__all__ = [
+    'PBHandler', 'Listener', 'PBController', 'ServiceStub', 'StubManager',
+    'Void', 'Controller', 'ErrorMessage'
+]
