c8b4c530a8cc1ba6dce74c8a2b27ea62f6c251fd
Catstyle <Catstyle.Lee@gmail.com>
2017-04-06 18:07:09 +0800

    moved handle_request/handle_notification to Listener

--- pymaid/pb/handler.py
+++ pymaid/pb/handler.py
@@ -5,28 +5,24 @@ from gevent.queue import Queue
 from google.protobuf.message import DecodeError
 
 from pymaid.conf import settings
-from pymaid.error import BaseEx, Error, RpcError
+from pymaid.error import RpcError
 from pymaid.error import get_exception
 from pymaid.utils import greenlet_pool
 
 from .controller import Controller
 from .listener import Listener
 from .stub import StubManager
-from .pymaid_pb2 import Void, ErrorMessage, Controller as PBC
+from .pymaid_pb2 import ErrorMessage, Controller as PBC
 
 
 class PBHandler(object):
 
     HEADER = '!HH'
-    header_length = struct.calcsize(HEADER)
-    header_struct = struct.Struct(HEADER)
-    pack_header = header_struct.pack
-    unpack_header = header_struct.unpack
+    unpack_header = struct.Struct(HEADER).unpack
 
     def __init__(self, listener=None, close_conn_onerror=True):
         self.listener = listener or Listener()
         self.close_conn_onerror = close_conn_onerror
-        self._get_rpc = self.listener.service_methods.get
 
     def __call__(self, conn):
         if not conn.oninit():
@@ -37,16 +33,16 @@ class PBHandler(object):
         gr = greenlet_pool.spawn(self.sequential_worker, tasks_queue)
         gr.link_exception(conn.close)
 
-        header_length = self.header_length
         RESPONSE = PBC.RESPONSE
         callbacks = {
-            PBC.REQUEST: self.handle_request,
-            PBC.NOTIFICATION: self.handle_notification,
+            PBC.REQUEST: self.listener.handle_request,
+            PBC.NOTIFICATION: self.listener.handle_notification,
         }
-        handle_response = self.handle_response
+        transmissions = conn.transmissions
+        responses = StubManager.response_class
         try:
             while 1:
-                header = conn.read(header_length)
+                header = conn.read(4)
                 if not header:
                     conn.close(reset=True)
                     break
@@ -62,8 +58,26 @@ class PBHandler(object):
                 controller = Controller(meta, conn)
                 content = buf[packet_length:]
                 packet_type = meta.packet_type
+
                 if packet_type == RESPONSE:
-                    handle_response(controller, content)
+                    result = transmissions.pop(meta.transmission_id, None)
+                    if not result:
+                        # invalid transmission_id, do nothing
+                        continue
+
+                    if meta.is_failed:
+                        try:
+                            message = ErrorMessage.FromString(content)
+                            ex = get_exception(message.code, message.message)
+                        except (DecodeError, ValueError) as ex:
+                            ex = ex
+                        result.set_exception(ex)
+                    else:
+                        try:
+                            result.set(responses[meta.service_method]
+                                       .FromString(content))
+                        except (DecodeError, ValueError) as ex:
+                            result.set_exception(ex)
                 else:
                     new_task((callbacks[packet_type], controller, content))
         except socket_error as ex:
@@ -83,82 +97,3 @@ class PBHandler(object):
                 break
             callback, controller, content = task
             callback(controller, content)
-
-    def handle_request(self, controller, content):
-        meta = controller.meta
-        meta.packet_type = PBC.RESPONSE
-        service_method = meta.service_method
-
-        conn = controller.conn
-        rpc = self._get_rpc(service_method)
-        if not rpc:
-            meta.is_failed = True
-            err = RpcError.RPCNotExist(service_method=service_method)
-            packet = ErrorMessage(code=err.code, message=err.message)
-            conn.send(b'{}{}{}'.format(
-                self.pack_header(meta.ByteSize(), packet.ByteSize()),
-                meta.SerializeToString(), packet.SerializeToString()
-            ))
-            return
-
-        method, request_class, response_class = rpc
-
-        def send_response(response=None, **kwargs):
-            if response_class is Void:
-                # do not send_response when response_class is Void
-                return
-            packet = response or response_class(**kwargs)
-            conn.send(b'{}{}{}'.format(
-                self.pack_header(meta.ByteSize(), packet.ByteSize()),
-                meta.SerializeToString(), packet.SerializeToString()
-            ))
-
-        request = request_class.FromString(content)
-        try:
-            method(controller, request, send_response)
-        except BaseEx as ex:
-            meta.is_failed = True
-            packet = ErrorMessage(code=ex.code, message=ex.message)
-            conn.send(b'{}{}{}'.format(
-                self.pack_header(meta.ByteSize(), packet.ByteSize()),
-                meta.SerializeToString(), packet.SerializeToString()
-            ))
-            if isinstance(ex, Error) and self.close_conn_onerror:
-                conn.delay_close(ex)
-
-    def handle_notification(self, controller, content):
-        service_method = controller.meta.service_method
-        rpc = self._get_rpc(service_method)
-        if not rpc:
-            # failed silently when handle_notification
-            return
-
-        method, request_class, response_class = rpc
-        request = request_class.FromString(content)
-        try:
-            method(controller, request, lambda *args, **kwargs: '')
-        except BaseEx:
-            # failed silently when handle_notification
-            pass
-
-    def handle_response(self, controller, content):
-        conn, meta = controller.conn, controller.meta
-        transmission_id = meta.transmission_id
-        async_result = conn.transmissions.pop(transmission_id, None)
-        if not async_result:
-            # invalid transmission_id, do nothing
-            return
-
-        if meta.is_failed:
-            try:
-                message = ErrorMessage.FromString(content)
-                ex = get_exception(message.code, message.message)
-            except (DecodeError, ValueError) as ex:
-                ex = ex
-            async_result.set_exception(ex)
-        else:
-            response_cls = StubManager.response_class[meta.service_method]
-            try:
-                async_result.set(response_cls.FromString(content))
-            except (DecodeError, ValueError) as ex:
-                async_result.set_exception(ex)
--- pymaid/pb/listener.py
+++ pymaid/pb/listener.py
@@ -1,7 +1,15 @@
+import struct
+
+from pymaid.error import BaseEx, Error, RpcError
+
+from .pymaid_pb2 import Void, ErrorMessage, Controller as PBC
+
 __all__ = ['Listener']
 
 
 class Listener(object):
+
+    pack_header = struct.Struct('!HH').pack
 
     def __init__(self):
         self.service_methods = {}
@@ -19,3 +27,60 @@ class Listener(object):
             service_methods[full_name] = tuples
             # js/lua pb lib will format as '.service.method'
             service_methods['.' + full_name] = tuples
+
+    def handle_request(self, controller, content):
+        meta = controller.meta
+        meta.packet_type = PBC.RESPONSE
+        service_method = meta.service_method
+
+        conn = controller.conn
+        rpc = self.service_methods.get(service_method)
+        if not rpc:
+            meta.is_failed = True
+            err = RpcError.RPCNotExist(service_method=service_method)
+            packet = ErrorMessage(code=err.code, message=err.message)
+            conn.send(b'{}{}{}'.format(
+                self.pack_header(meta.ByteSize(), packet.ByteSize()),
+                meta.SerializeToString(), packet.SerializeToString()
+            ))
+            return
+
+        method, request_class, response_class = rpc
+
+        def send_response(response=None, **kwargs):
+            if response_class is Void:
+                # do not send_response when response_class is Void
+                return
+            packet = response or response_class(**kwargs)
+            conn.send(b'{}{}{}'.format(
+                self.pack_header(meta.ByteSize(), packet.ByteSize()),
+                meta.SerializeToString(), packet.SerializeToString()
+            ))
+
+        request = request_class.FromString(content)
+        try:
+            method(controller, request, send_response)
+        except BaseEx as ex:
+            meta.is_failed = True
+            packet = ErrorMessage(code=ex.code, message=ex.message)
+            conn.send(b'{}{}{}'.format(
+                self.pack_header(meta.ByteSize(), packet.ByteSize()),
+                meta.SerializeToString(), packet.SerializeToString()
+            ))
+            if isinstance(ex, Error) and self.close_conn_onerror:
+                conn.delay_close(ex)
+
+    def handle_notification(self, controller, content):
+        service_method = controller.meta.service_method
+        rpc = self.service_methods.get(service_method)
+        if not rpc:
+            # failed silently when handle_notification
+            return
+
+        method, request_class, response_class = rpc
+        request = request_class.FromString(content)
+        try:
+            method(controller, request, lambda *args, **kwargs: '')
+        except BaseEx:
+            # failed silently when handle_notification
+            pass
