666c938634e4263a6faa0f795a51e3992f24c25b
Catstyle <Catstyle.Lee@gmail.com>
2017-04-07 20:25:21 +0800

    added hash.primes const tuple; refined MaglevHash lookup_table_size

--- pymaid/utils/hash.py
+++ pymaid/utils/hash.py
@@ -2,6 +2,21 @@ from hashlib import md5
 from bisect import bisect
 
 
+primes = (
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151,
+    157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233,
+    239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317,
+    331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419,
+    421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503,
+    509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607,
+    613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701,
+    709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811,
+    821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911,
+    919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997
+)
+
+
 def md5_hash_func(key):
     return int(md5(key).hexdigest(), 16)
 
@@ -98,9 +113,10 @@ class HashRing(BaseHashManager):
 
 class MaglevHash(BaseHashManager):
 
-    def __init__(self, lookup_table_size, hash_func=md5_hash_func):
+    def __init__(self, hash_func=md5_hash_func, virtual_entry_count=16):
         super(HashRing, self).__init__(hash_func)
-        self.lookup_table_size = lookup_table_size
+        self.virtual_entry_count = virtual_entry_count
+        self.lookup_table_size = 0
         self.lookup_table = {}
 
     def rehash(self):
@@ -109,18 +125,21 @@ class MaglevHash(BaseHashManager):
 
         permutation = []
         hash_func = self.hash_func
-        lookup_table_size = self.lookup_table_size
+        entry_count = len(self.nodes) * self.virtual_entry_count
+        pos = bisect(primes, entry_count)
+        entry_count = primes[pos if pos < len(primes) else -1]
+        self.lookup_table_size = entry_count
         for node in self.nodes:
             key = node.key
-            offset = hash_func('cat' + key) % lookup_table_size
-            skip = (hash_func('lee' + key) % (lookup_table_size - 1)) + 1
+            offset = hash_func('cat' + key) % entry_count
+            skip = (hash_func('lee' + key) % (entry_count - 1)) + 1
             permutation.append([
-                (offset + idx * skip) % lookup_table_size
-                for idx in range(lookup_table_size)
+                (offset + idx * skip) % entry_count
+                for idx in range(entry_count)
             ])
 
         nexts = [0] * len(self.nodes)
-        entries = self.lookup_table = [-1] * lookup_table_size
+        entries = self.lookup_table = [-1] * entry_count
 
         n = 0
         while 1:
@@ -133,7 +152,7 @@ class MaglevHash(BaseHashManager):
                 nexts[idx] += 1
                 n += 1
 
-                if n == lookup_table_size:
+                if n == entry_count:
                     return
 
     def get_node_by_key(self, key):
