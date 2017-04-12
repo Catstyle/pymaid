31b7a8a33c7f7e2a81a31ff5991d4313c5822823
Catstyle <Catstyle.Lee@gmail.com>
2017-04-12 14:44:22 +0800

    fixed examples clients to handle rpc result

--- examples/reraise/client.py
+++ examples/reraise/client.py
@@ -12,7 +12,7 @@ def wrapper(pid, n):
     global cnt
     for x in range(n):
         try:
-            service.player_profile(conn=conn, user_id=x)
+            service.player_profile(conn=conn, user_id=x).get(30)
         except PlayerNotExist:
             cnt += 1
         else:
