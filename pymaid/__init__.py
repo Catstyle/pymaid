ad25edc24409e5e534411167537f95ea2289907a
Catstyle <Catstyle.Lee@gmail.com>
2017-05-08 14:50:26 +0800

    bump version 0.3.7b1

--- pymaid/__init__.py
+++ pymaid/__init__.py
@@ -15,7 +15,7 @@ __all__ = [
     'conf', 'serve_forever'
 ]
 
-__version__ = '0.3.7a2'
+__version__ = '0.3.7b1'
 
 if 'linux' in sys.platform or 'darwin' in sys.platform:
     # if 'ares' not in os.environ.get('GEVENT_RESOLVER', ''):
