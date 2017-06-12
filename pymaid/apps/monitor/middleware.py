a959f64a5bab20457420d49484380f7148a434e9
Catstyle <Catstyle.Lee@gmail.com>
2017-06-12 23:31:17 +0800

    fixed heartbeat_timer, stop it in callback; donot use greenlet

--- pymaid/apps/monitor/middleware.py
+++ pymaid/apps/monitor/middleware.py
@@ -20,9 +20,10 @@ class MonitorMiddleware(BaseMiddleware):
         def heartbeat_timeout():
             conn.heartbeat_count += 1
             if conn.heartbeat_count >= self.heartbeat_count:
+                conn.heartbeat_timer.stop()
                 conn.close(MonitorError.HeartbeatTimeout())
 
-        heartbeat_timer = timer(0, self.heartbeat_interval, use_greenlet=True)
+        heartbeat_timer = timer(0, self.heartbeat_interval)
         heartbeat_timer.again(heartbeat_timeout)
         conn.heartbeat_timer = heartbeat_timer
         conn.clear_heartbeat_counter = clear_heartbeat_counter
