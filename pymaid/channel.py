87077a847e9b1edb6b24323d604149e88ba1fa19
Catstyle <Catstyle.Lee@gmail.com>
2017-06-22 19:43:06 +0800

    added ServerChannel.pause reason

--- pymaid/channel.py
+++ pymaid/channel.py
@@ -82,7 +82,7 @@ class ServerChannel(BaseChannel):
             if cnt >= settings.MAX_ACCEPT:
                 break
             if self.is_full:
-                self.pause()
+                self.pause('stop accept since channel is full')
                 break
             cnt += 1
             try:
@@ -138,14 +138,15 @@ class ServerChannel(BaseChannel):
             if not watcher.active:
                 watcher.start(self._do_accept, sock)
 
-    def pause(self):
+    def pause(self, reason):
+        self.logger.info('calling pause with reason: %s', reason)
         self.is_paused = True
         for watcher, sock in self.accept_watchers:
             if watcher.active:
                 watcher.stop()
 
     def stop(self, reason='ServerChannel calls stop'):
-        self.pause()
+        self.pause(reason)
         super(ServerChannel, self).stop(reason)
 
 
