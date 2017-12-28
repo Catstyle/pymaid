1fe60f74c3c2e0c9ccf036abf3069ef5311a1632
Catstyle <Catstyle.Lee@gmail.com>
2017-12-28 16:57:30 +0800

    fixed pb.Listener ErrorManager add_warning issue; refined monitor error

--- pymaid/apps/monitor/error.py
+++ pymaid/apps/monitor/error.py
@@ -1,7 +1,7 @@
-from pymaid.error import create_manager
+from pymaid.error import ErrorManager
 
 
-MonitorError = create_manager('MonitorError', 13580)
-MonitorError.build_error(
+MonitorError = ErrorManager.create_manager('MonitorError', 13580)
+MonitorError.add_error(
     'HeartbeatTimeout', 1, 'has not received heartbeat notification in time'
 )
