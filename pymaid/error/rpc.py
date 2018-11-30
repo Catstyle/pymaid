c91769a4dfdc7476ce72c7eca65e971feb4d08fd
Catstyle <Catstyle.Lee@gmail.com>
2018-11-30 14:52:07 +0800

    error: added python 2/3 representation compability

--- pymaid/error/rpc.py
+++ pymaid/error/rpc.py
@@ -1,13 +1,16 @@
+from six import text_type
+
 from .base import ErrorManager
 
 
 RpcError = ErrorManager.create_manager('RpcError', 13570)
-RpcError.add_error('RPCNotExist', 1, '[rpc|{}] not found')
+RpcError.add_error('RPCNotExist', 1, text_type('[rpc|{}] not found'))
 RpcError.add_error(
-    'HeartbeatTimeout', 2, '[host|{}][peer|{}] peer heartbeat timeout'
+    'HeartbeatTimeout', 2,
+    text_type('[host|{}][peer|{}] peer heartbeat timeout')
 )
 RpcError.add_error(
-    'PacketTooLarge', 3, '[packet_length|{}] out of limitation'
+    'PacketTooLarge', 3, text_type('[packet_length|{}] out of limitation')
 )
 RpcError.add_error('EOF', 4, 'socket received eof')
-RpcError.add_error('Timeout', 5, 'socket read/readline timeout {}')
+RpcError.add_error('Timeout', 5, text_type('socket read/readline timeout {}'))
