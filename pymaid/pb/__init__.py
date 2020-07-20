30f5e91bd546fe12b2d87fbb9e825952f7d07873
Catstyle <Catstyle.Lee@gmail.com>
2020-07-20 16:46:45 +0800

    updated requirements; updated pre-commit hooks; refined codes

--- pymaid/pb/__init__.py
+++ pymaid/pb/__init__.py
@@ -2,16 +2,16 @@ from struct import Struct
 
 from pymaid.conf import settings
 
-st = Struct(settings.get('PM_PB_HEADER', ns='pymaid'))  # noqa
-header_size = st.size  # noqa
-pack_header = st.pack  # noqa
-unpack_header = st.unpack  # noqa
+st = Struct(settings.get('PM_PB_HEADER', ns='pymaid'))
+header_size = st.size
+pack_header = st.pack
+unpack_header = st.unpack
 
-from .controller import Controller as PBController
-from .handler import PBHandler
-from .listener import Listener
-from .pymaid_pb2 import Void, Controller, ErrorMessage
-from .stub import ServiceStub, StubManager
+from .controller import Controller as PBController  # noqa
+from .handler import PBHandler  # noqa
+from .listener import Listener  # noqa
+from .pymaid_pb2 import Void, Controller, ErrorMessage  # noqa
+from .stub import ServiceStub, StubManager  # noqa
 
 
 __all__ = [
