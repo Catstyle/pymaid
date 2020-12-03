c94e67d847549618ec1f5ba8108cc9bc043b4c69
Catstyle <Catstyle.Lee@gmail.com>
2020-12-03 22:15:10 +0800

    reverted pymaid.ext.middleware; removed pyproject.toml

--- pymaid/ext/middleware.py
+++ pymaid/ext/middleware.py
@@ -1,4 +1,15 @@
-class BaseMiddleware(object):
+from collections import defaultdict
+from typing import List, Optional
+
+
+class BaseMiddleware:
+    '''Base class for Middleware.
+
+    *NOTE*: all on_* methods will be treated as event callbacks, and will be cached in
+        MiddlewareManager.events for faster dispatching.
+
+    e.g.: on_connect/on_close are examples.
+    '''
 
     def on_connect(self, conn):
         raise NotImplementedError()
@@ -9,5 +20,24 @@ class BaseMiddleware(object):
 
 class MiddlewareManager:
 
+    def __init__(self, middlewares: Optional[List[BaseMiddleware]] = None):
+        self.middlewares = []
+        self.events = defaultdict(list)
+
+        if middlewares:
+            self.append_middlewares(middlewares)
+
+    def append_middleware(self, middleware: BaseMiddleware):
+        self.middlewares.append(middleware)
+
+        for attr in dir(middleware):
+            if attr.startswith('on_'):
+                self.events[attr].append(getattr(middleware, attr))
+
+    def append_middlewares(self, middlewares: List[BaseMiddleware]):
+        for mw in middlewares:
+            self.append_middleware(mw)
+
     def dispatch(self, event, *args, **kwargs):
-        pass
+        for callback in self.events.get(event, []):
+            callback(*args, **kwargs)
