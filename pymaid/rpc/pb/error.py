21967162ab5c5be6e561ea7bacf016a6920fe4c7
Catstyle <Catstyle.Lee@gmail.com>
2021-01-08 11:36:42 +0800

    rpc.pb.error: refined PBError.PacketTooLarge

--- pymaid/rpc/pb/error.py
+++ pymaid/rpc/pb/error.py
@@ -6,7 +6,7 @@ PBError.add_error('RPCNotFound', 'rpc not found')
 PBError.add_error(
     'HeartbeatTimeout', '[host|{}][peer|{}] peer heartbeat timeout'
 )
-PBError.add_error('PacketTooLarge', '[packet_length|{}] out of limitation')
+PBError.add_error('PacketTooLarge', 'packet_length out of limitation')
 PBError.add_error('EOF', 'socket received eof')
 PBError.add_error('Timeout', 'socket read/readline timeout {}')
 PBError.add_error('InvalidTransmissionID', 'transmission_id value is invalid')
