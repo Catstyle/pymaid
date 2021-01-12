ccae52d5e4f5ca11b5b216f4cdfc94197ef5103b
Catstyle <Catstyle.Lee@gmail.com>
2021-01-12 15:28:55 +0800

    examples: separated asyncio/pymaid examples

---
+++ examples/echo_asynctio/asyncio_stream_server.py
@@ -0,0 +1,39 @@
+import asyncio
+import socket
+
+from examples.template import get_server_parser, parse_args
+
+
+async def handler(reader, writer):
+    args.debug(f'[conn][reader|{reader}][writer|{writer}] connected')
+    sock = writer.get_extra_info('socket')
+    try:
+        sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    except (OSError, NameError) as ex:
+        args.debug(f'set nodelay failed {ex}')
+        pass
+    read, write = reader.read, writer.write
+    while 1:
+        data = await read(256 * 1024)
+        if not data:
+            break
+        write(data)
+    await writer.drain()
+    writer.close()
+    await writer.wait_closed()
+    args.debug(f'[conn][reader|{reader}][writer|{writer}] closed')
+
+
+async def main(args):
+    if isinstance(args.address, str):
+        channel = await asyncio.start_unix_server(handler, args.address)
+    else:
+        channel = await asyncio.start_server(handler, *args.address)
+
+    async with channel:
+        await channel.serve_forever()
+
+
+if __name__ == "__main__":
+    args = parse_args(get_server_parser())
+    asyncio.run(main(args))
--- examples/template.py
+++ examples/template.py
@@ -2,37 +2,42 @@ from argparse import ArgumentParser
 import re
 
 
-def get_server_parser():
+def get_base_parser():
     parser = ArgumentParser()
     parser.add_argument(
         '--address', type=str, default='/tmp/pymaid.sock',
-        help='listen address',
+        help='transport address',
     )
-    # parser.add_argument(
-    #     '--uvloop', action='store_true', default=False, help='use uvloop'
-    # )
+    parser.add_argument(
+        '--uvloop', action='store_true', default=False, help='use uvloop'
+    )
+    parser.add_argument(
+        '-v', '--verbose', action='store_true', default=False,
+        help='print debug info'
+    )
+    return parser
+
+
+def get_server_parser():
+    parser = get_base_parser()
+    parser.add_argument(
+        '--backlog', type=int, default=1024, help='listen backlog',
+    )
     return parser
 
 
 def get_client_parser():
-    parser = ArgumentParser()
+    parser = get_base_parser()
     parser.add_argument(
         '-c', dest='concurrency', type=int, default=100, help='concurrency',
     )
     parser.add_argument(
         '-r', dest='request', type=int, default=100, help='request per client',
     )
+    parser.add_argument('--msize', type=int, default=1024, help='message size')
     parser.add_argument(
-        '--address', type=str, default='/tmp/pymaid.sock',
-        help='connect address',
+        '--timeout', type=int, default=60, help='timeout, in seconds',
     )
-    parser.add_argument(
-        '--timeout', type=int, default=60,
-        help='timeout, in seconds',
-    )
-    # parser.add_argument(
-    #     '--uvloop', action='store_true', default=False, help='use uvloop',
-    # )
     return parser
 
 
@@ -42,7 +47,13 @@ def parse_args(parser):
         address, port = args.address.split(':')
         args.address = (address, int(port))
     print(args)
-    # if args.uvloop:
-    #     import uvloop
-    #     uvloop.install()
+    if args.uvloop:
+        import uvloop
+        uvloop.install()
+    if args.verbose:
+        args.debug = print
+    else:
+        def debug(*args, **kwargs):
+            pass
+        args.debug = debug
     return args
